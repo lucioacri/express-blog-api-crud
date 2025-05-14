5207c83c8f87404b4fcbd54be8b2b566da35e3c0
lucioacri <lucioacri@gmail.com>
2025-05-14 13:44:12 +0200

    changed destroy

--- controllers/postscontroller.js
+++ controllers/postscontroller.js
@@ -1,6 +1,14 @@
 const { posts } = require("../data.js");
 
 const index = (req, res) => {
+  let filteredPosts = [...posts];
+  if (req.query.tags) {
+    filteredPosts = posts.filter((post) => post.tags.includes(req.query.tags));
+    return res.json({
+      description: "Post filtrato",
+      object: filteredPosts,
+    });
+  }
   res.json({
     Description: "Lista dei post",
     Object: posts,
@@ -28,16 +36,25 @@ const create = (req, res) => {
 const update = (req, res) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res
+      .status(404)
+      .json({ description: "Post not found", object: "Error 404" });
+  }
   res.json({ Description: "Aggiornamento del post " + id, Object: "" });
 };
 
 const destroy = (req, res) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
-  res.json({
-    Description: "Cancellazione del post " + id,
-    Object: posts.filter((post) => post.id !== id),
-  });
+  if (!post) {
+    return res
+      .status(404)
+      .json({ description: "Post not found", object: "Error 404" });
+  }
+  posts.splice(posts.indexOf(post), 1);
+  res.sendStatus(204);
+  console.log(posts);
 };
 
 module.exports = { index, show, create, update, destroy };
