0accd76032d5b215cd87f4f9981efc29df590848
lucioacri <lucioacri@gmail.com>
2025-05-16 15:46:43 +0200

    add 404 notFound middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const routerPosts = require("./routers/posts");
 const errorHandler = require("./middlewares/errorHandler");
+const notFound = require("./middlewares/notFound");
 
 app.listen(port, () => {
   console.log("Server collgato alla port:" + port);
@@ -21,3 +22,4 @@ app.use("/bacheca", routerPosts);
 
 // Errors middlewares
 app.use(errorHandler);
+app.use(notFound);
